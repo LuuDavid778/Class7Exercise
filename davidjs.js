5e02ffabc4371ffdb6ba8a3e5deeddefd7ff1412
luudavid778 <luudavid778@gmail.com>
2020-02-20 16:07:57 -0800

    David's FIles

---
+++ davidjs.js
@@ -0,0 +1,60 @@
+var data = {
+  qnum: 0,
+  questions: [
+    {
+      title: "What is a dog?",
+      answer: "mammal",
+      bgcolor: "yellow"
+    },
+    {
+      title: "What is an ant?",
+      answer: "insect",
+      bgcolor: "brown"
+    },
+    {
+      title: "What is a shark?",
+      answer: "fish",
+      bgcolor: "blue"
+    },
+    {
+      title: "Are apples considered fruits?",
+      answer: "yes",
+      bgcolor: "coral"
+    },
+    {
+      title: "Are potatos considered fruits?",
+      answer: "no",
+      bgcolor: "brown"
+    },
+    {
+      title: "What country has the biggest population?",
+      answer: "China",
+      bgcolor: "red"
+    }
+  ]
+};
+
+var qnum = 0;
+
+function start() {
+  document.querySelector("#app__question__h1").innerText =
+    data.questions[data.qnum].title;
+  document.body.style.backgroundColor = data.questions[data.qnum].bgcolor;
+}
+
+function CheckAnswer() {
+  var myinp = document.querySelector("#useranswer").value;
+
+  if (myinp === data.questions[data.qnum].answer) {
+    document.querySelector("#app__results").innerHTML = "<h1>Correct!</h1>";
+    data.qnum++;
+    document.querySelector("#app__question__h1").innerText =
+      data.questions[data.qnum].title;
+    document.body.style.backgroundColor = data.questions[data.qnum].bgcolor;
+  } else {
+    document.querySelector("#app__results").innerHTML = "<h1>Incorrect!</h1>";
+    // alert("Wrong!")
+  }
+}
+
+start();
