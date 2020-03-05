5400dc7b50db051d1e5887d69062ac7273a9518c
David Luu <a01172778@se14l0120-01.ad.bcit.ca>
2020-03-05 14:56:27 -0800

    Added healthbar

---
+++ healthbar/healthjava.js
@@ -0,0 +1,18 @@
+var health_state = {
+
+    width:80
+
+}
+
+
+function ChangeHealth() {
+if(health_state.width > 0) {
+    health_state.width = health_state.width - 10;
+  document.getElementById("hpbar__bar").style.width = health_state.width + "%";
+    console.log(health_state.width)
+}
+
+    
+}
+
+
